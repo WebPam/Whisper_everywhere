818420d3feba1203cfd044cc1ac6f89c5640833d
Laurent BONNIER <lbonnier@web-interaction.fr>
2023-09-04 16:50:59 +0200

    Update background.js

--- public/background.js
+++ public/background.js
@@ -1,9 +1,20 @@
-// eslint-disable-next-line no-undef
+// Écouteur pour l'événement d'installation de l'extension.
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed!');
 });
 
-// eslint-disable-next-line no-undef
+// Écouteur pour les messages envoyés à l'arrière-plan.
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message);
+
+    // Ici, vous pouvez ajouter une logique supplémentaire pour gérer différents types de messages.
+    // Par exemple :
+    // if (message.type === 'someMessageType') {
+    //     // Traitez le message de ce type ici.
+    //     sendResponse({ response: 'Response for someMessageType' });
+    // }
+
+    // Notez que si vous utilisez sendResponse, vous devrez peut-être retourner true 
+    // pour indiquer que la réponse sera envoyée de manière asynchrone.
+    // return true;
 });
